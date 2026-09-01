c8702e47160647fadb92168f4960e606e3f9cf8f
mahmunirmoon <DRMahMonir@proton.me>
2026-09-01 09:18:09 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { HelpWidget } from "./components/HelpWidget";
+import { StudentCredit } from "./components/StudentCredit";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <StudentCredit />
+    <HelpWidget />
+  </React.StrictMode>,
+);
